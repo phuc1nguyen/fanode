951b4d20c1bf0032567b511d04b571b710227372
phuc1nguyen <phuc.ng13988@gmail.com>
2023-03-22 10:40:22 +0700

    Define models

--- src/models/Customer.js
+++ src/models/Customer.js
@@ -1,8 +1,26 @@
 import { Model } from 'objection';
+import Employee from './Employee.js';
 
 class Customer extends Model {
   static get tableName() {
     return 'customers';
+  }
+
+  static get idColumn() {
+    return 'customerNumber';
+  }
+
+  static get relationMappings() {
+    return {
+      employees: {
+        relations: Model.BelongsToOneRelation,
+        modelClass: Employee,
+        join: {
+          from: 'customers.salesRepEmployeeNumber',
+          to: 'employees.employeeNumber',
+        },
+      },
+    };
   }
 }
 
--- src/models/Employee.js
+++ src/models/Employee.js
@@ -1,8 +1,26 @@
 import { Model } from 'objection';
+import Customer from './Customer.js';
 
 class Employee extends Model {
   static get tableName() {
     return 'employees';
+  }
+
+  static get idColumn() {
+    return 'employeeNumber';
+  }
+
+  static get relationMappings() {
+    return {
+      customers: {
+        relations: Model.HasManyRelation,
+        modelClass: Customer,
+        join: {
+          from: 'employees.employeeNumber',
+          to: 'customers.salesRepEmployeeNumber',
+        },
+      },
+    };
   }
 }
 
